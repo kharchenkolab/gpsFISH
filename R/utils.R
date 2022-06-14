c905e7ae1123f8717cbec610a4ff564b2f49ff79
Yida Zhang <yzhang@bayes.med.harvard.edu>
2022-06-14 13:14:54 -0400

    add function densityscatter

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,64 @@ relative_freq=function(count_matrix, gene_list, cluster_label, cell_cluster_conv
   proportion=rowSums(as.matrix(count_matrix[gene_list,cells_in_cluster]))/total_count
   return(proportion)
 }
+
+
+#' A scatterplot with color corresponding to density
+#'
+#' @description Visualize two dimensional data using scatterplot with density information
+#' @param x A numeric vector containing the \code{x} coordinates of points in the plot
+#' @param y A numeric vector containing the \code{y} coordinates of points in the plot
+#' @param xlab a title for the x axis
+#' @param ylab a title for the y axis
+#' @param main an overall title for the plot
+#' @param add.diagonal a logical value indicating whether you want to add \code{x = y} line to the plot
+#' @param x_low A numeric value representing the lower limit of \code{x} coordinate
+#' @param x_high A numeric value representing the upper limit of \code{x} coordinate
+#' @param y_low A numeric value representing the lower limit of \code{y} coordinate
+#' @param y_high A numeric value representing the upper limit of \code{y} coordinate
+#'
+#' @export
+#'
+#' @examples
+#' require(ggplot2)
+#' x = rnorm(1000, mean = 0, sd = 1)
+#' y = rnorm(1000, mean = 0, sd = 1)
+#' densityscatter(x, y, xlab = "x axis", ylab = "y axis", main = "example plot", add.diagonal = TRUE,
+#'                x_low = 0, x_high = 3, y_low = 0, y_high = 3)
+densityscatter=function(x, y, xlab = NULL, ylab = NULL, main = NULL, add.diagonal = F,
+                        x_low = NULL, x_high = NULL, y_low = NULL, y_high = NULL){
+  df=data.frame(x=x, y=y)
+  if (is.null(x_low)){
+    x_low = floor(min(df$x))
+  }
+  if (is.null(x_high)){
+    x_high = ceiling(max(df$x))
+  }
+  if (is.null(y_low)){
+    y_low = floor(min(df$y))
+  }
+  if (is.null(y_high)){
+    y_high = ceiling(max(df$y))
+  }
+  # y_high = 0
+  p = ggplot2::ggplot(data = df, mapping = ggplot2::aes(x = x, y = y)) +
+    ggpointdensity::geom_pointdensity() +
+    viridis::scale_color_viridis() +
+    ggplot2::xlab(xlab) + ggplot2::ylab(ylab) + ggplot2::ggtitle(main) + ggplot2::xlim(x_low, x_high) + ggplot2::ylim(y_low, y_high) +
+    ggplot2::theme(
+      # axis
+      axis.title.x = ggplot2::element_text(face="bold", size=16, margin = ggplot2::margin(t = 10, r = 0, b = 0, l = 0)),
+      axis.title.y = ggplot2::element_text(face="bold", size=16, margin = ggplot2::margin(t = 0, r = 10, b = 0, l = 0), angle=90),
+      axis.text = ggplot2::element_text(size = ggplot2::rel(1.1)),
+      axis.text.x = ggplot2::element_text(hjust = 0.5, vjust = 0, size=18),
+      axis.text.y = ggplot2::element_text(vjust = 0.5, hjust = 0, size=18),
+      axis.line = ggplot2::element_line(colour = "black"),
+      #background
+      panel.background = ggplot2::element_blank()
+    )
+  if (add.diagonal){
+    p = p + ggplot2::geom_abline(intercept = 0, slope = 1)
+  }
+  print(p)
+}
+
