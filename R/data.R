442f1ce466329db5368c66e0ae32a75430887063
Yida Zhang <yzhang@bayes.med.harvard.edu>
2022-06-16 18:05:08 -0400

    add simulation_params dataset

--- R/data.R
+++ R/data.R
@@ -5,6 +5,7 @@
 #' @format A count matrix with 19,972 rows and 550 columns:
 "sc_count"
 
+
 #' scRNA-seq count data cell annotation.
 #'
 #' A dataset containing the cell type label of the 550 cells in the scRNA-seq data.
@@ -13,9 +14,9 @@
 #' \describe{
 #'   \item{cell_name}{name of each cell}
 #'   \item{class_label}{annotated cell type of each cell}
-#'   ...
 #' }
 "sc_cluster"
+
 
 #' Spatial transcriptomics count data.
 #'
@@ -23,6 +24,7 @@
 #'
 #' @format A count matrix with 33 rows and 537 columns:
 "spatial_count"
+
 
 #' Spatial transcriptomics count data cell annotation.
 #'
@@ -32,6 +34,22 @@
 #' \describe{
 #'   \item{cell_name}{name of each cell}
 #'   \item{class_label}{annotated cell type of each cell}
-#'   ...
 #' }
 "spatial_cluster"
+
+
+#' Pre-trained Bayesian model based on the Codeluppi dataset.
+#'
+#' A list object returned by the simulation_training_ZINB function and then trimmed by the simulation_training_ZINB_trim function. The model is trained on the Codeluppi dataset.
+#'
+#' @format A list with 7 slots:
+#' \describe{
+#'   \item{data2fit}{A data frame containing the input data for fitting the Bayesian model.}
+#'   \item{model.summary}{A named list with elements \code{summary} and \code{c_summary}, which contain summaries of a stanfit object.}
+#'   \item{fixed.effect}{A named list with elements \code{summary} and \code{c_summary}, which contain summaries of specified parameters.}
+#'   \item{c_i_full}{A matrix containing the estimated gene specific intercept \eqn{c_i} for each gene.}
+#'   \item{gamma_i_full}{A matrix containing the estimated gene specific coefficient \eqn{\gamma_i} for each gene.}
+#'   \item{lib.size}{A numeric vector containing the library size, i.e., total number of molecule per cell, of cells in the spatial transcriptomics data.}
+#'   \item{posterior}{A list containing the extracted samples of variables in the Bayesian model from their posterior distribution.}
+#' }
+"simulation_params"
