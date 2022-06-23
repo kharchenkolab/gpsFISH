906575997a4d25c1cd287a8a35e0a604dae56654
Yida Zhang <yzhang@bayes.med.harvard.edu>
2022-06-23 18:43:11 -0400

    add new function: plot_norm_confusion_matrix

--- R/utils.R
+++ R/utils.R
@@ -927,6 +927,23 @@ plot_confusion_matrix=function(confusion.matrix){
 }
 
 
+#' Plot normalized confusion matrix
+#'
+#' @param confusion.matrix A normalized confusion matrix returned by \code{gpsFISH_optimize}.
+#'
+#' @return ggplot2 object
+#' @export
+#'
+plot_norm_confusion_matrix=function(confusion.matrix){
+  d2p = reshape2::melt(confusion.matrix)
+  p = ggplot2::ggplot(d2p, ggplot2::aes(x=Reference, y=Prediction, fill=value)) + ggplot2::geom_tile() + ggplot2::theme_bw() + ggplot2::coord_equal() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) +
+    ggplot2::scale_fill_distiller(palette="Greens", direction=1) +
+    ggplot2::guides(fill=F) + # removing legend for `fill`
+    ggplot2::geom_text(ggplot2::aes(label=round(value, digit=2)*100), color="black", size = 3) # printing values
+  return(p)
+}
+
 
 #' Calculate weighted confusion matrix and accuracy
 #'
