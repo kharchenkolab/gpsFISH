fcb01ea912994f9b379561cb0b7fe72c4bb8c4b9
Yida Zhang <yzhang@bayes.med.harvard.edu>
2022-08-10 15:40:43 -0400

    updated document of MaxCol

--- src/get_max_index_per_row.cpp
+++ src/get_max_index_per_row.cpp
@@ -1,8 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// https://stackoverflow.com/questions/35047929/fastest-way-to-find-the-index-of-the-maximum-of-each-row/35048449#35048449
-
+//' Find the index of the maximum value of each row in a matrix.
+//' https://stackoverflow.com/questions/35047929/fastest-way-to-find-the-index-of-the-maximum-of-each-row/35048449#35048449
+//'
+//' @param m Numeric matrix
+//'
+//' @return A numeric vector containing the index for each row.
+//' @export
 // [[Rcpp::export]]
 Rcpp::NumericVector MaxCol(Rcpp::NumericMatrix m) {
   R_xlen_t nr = m.nrow(), nc = m.ncol(), i = 0;
